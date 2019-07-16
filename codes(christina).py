81a14f1d0a82a496f0e6afc5fb03cb81c152430a
Christina662 <52932066+Christina662@users.noreply.github.com>
2019-07-16 16:04:54 -0400

    Add files via upload

---
+++ codes(christina).py
@@ -0,0 +1,62 @@
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello world")
+print ("I am learning to code in python today")
+
+print ("123")
+print("hiiiiiii")
+"hello world"
+5*2
+5/2
+5-2
+5**2
+8/9*3
+print("5 divided by 2")
+5/2
+#create function to sum any two numbers
+
+def plus(a,b,c):
+    plus= a + b + c
+    print(plus)
+    
+plus(1,3,5)
+print(1/2*3*5)
+print(0/0)
+"whats good"
+print hola
+print ("box")
+       box=box+5 
+       
+       print(box)
+       
+#chocolate types 
+       d="dark"
+       m="milk"
+       w="white"
+       print (d/mw/w)
+       print(d,m,w) 
+
+"d"= dark 
+print("hi kaelem, how are you?")
+
+k= "kaelem"
+print("how are you")
+import math
+
+dir(math)
+math.pow(2,4)
+math.pow(2,1/4)
+math.pow(3,5/8)
+def hi(c):
+    hi = c
+    math.pow (c)1/3
+print ("c=35")
+ 
+def cubicroot(x):
+    cubicroot =x**(1/3)
+    print(cubicroot)
